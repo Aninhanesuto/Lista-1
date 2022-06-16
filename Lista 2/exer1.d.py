6c544822a67b97bee9e88c02386ae1b498abaf0c
Aninhanesuto <aninhasesna@gmail.com>
2022-06-16 15:55:20 -0300

    Lista 2

--- Lista 2/exer1.d.py
+++ Lista 2/exer1.d.py
@@ -3,10 +3,16 @@ def crescente(x1,x2,x3):
 		print(f"{x1}\n{x2}\n{x3}") 
 	elif x1<x3<x2:
 		print(f"{x1}\n{x3}\n{x2}") 
-def main():
-	x1 = int(input())
-	x2 = int(input())
-	x3 = int(input())
-	crescente(x1,x2,x3)
+	elif x2<x1<x3:
+		print(f"{x2}\n{x1}\n{x3}")
+	elif x2<x3<x1:
+		print(f"{x2}\n{x3}\n{x1}")
+	elif x3<x1<x2:
+		print(f"{x3}\n{x1}\n{x2}")
+	elif x3<x2<x1:
+		print(f"{x3}\n{x2}\n{x1}")
+x1 = int(input())
+x2 = int(input())
+x3 = int(input())
+crescente(x1,x2,x3)
 
-main()
