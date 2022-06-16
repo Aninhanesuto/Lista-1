ea865dbb0cccd273bd6c8cedaaedd4436049654d
Aninhanesuto <aninhasesna@gmail.com>
2022-06-16 18:06:43 -0300

    Lista 2

---
+++ Lista 2/exerc1.j.py
@@ -0,0 +1,10 @@
+def tipo(v):
+	if type(v) == int:
+		print("Inteiro")
+	elif type(v) == float:
+		print("Ponto Flutuante")
+	elif type(v) == str:
+		print("Str")
+	elif type(v) == bool:
+		print("Boleano")
+tipo(True)
